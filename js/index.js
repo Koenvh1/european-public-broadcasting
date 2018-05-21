8533a994649338e44df1fb46b93aaa6881878ea9
Koen <koen@koenvh.nl>
2018-05-21 17:54:48 +0200

    Switch to new API

--- js/index.js
+++ js/index.js
@@ -1,29 +1,5 @@
 async function load() {
-    let series = await (await fetch("https://cors-anywhere.herokuapp.com/https://apps-api.uitzendinggemist.nl/series.json")).json();
-
-    let content = "";
-
-    for(let i = 0; i < series.length; i++) {
-        let serie = series[i];
-
-        content += `
-            <div class="col-md-2 series-item" data-id="` + serie.mid + `">
-                    <div class="card mb-4 box-shadow series-show">
-                        <img class="card-img-top overview-image" src="` + serie.image + `" alt="Series image" onerror="this.src='https://placehold.it/400x200'">
-                        <div class="card-body">
-                            <h5 class="card-title overflow">` + serie.name + `</h5>
-                            <p class="card-text"></p>
-                            <div class="d-flex justify-content-between align-items-center">
-                                <small class="text-muted overflow">` + serie.broadcasters.join(", ") + `</small>
-                            </div>
-                        </div>
-                    </div>
-                </div>`
-    }
-
-    $("#series").html(content);
-
-    $(".series-show").on("click", async (event) => {
+    $(document).on("click", ".series-show", async (event) => {
         NProgress.start();
         let data = await (await fetch("https://cors-anywhere.herokuapp.com/https://apps-api.uitzendinggemist.nl/series/" + $(event.target).closest(".series-item").data("id") + ".json")).json();
         $(".series-image").attr("src", data["image"]);
@@ -47,7 +23,101 @@ async function load() {
         $(".series-episodes").html(episodeContent);
         $(".series-modal").modal("show");
         NProgress.done();
+    });
+
+    $(".filter").change((event) => {
+        filters[$(event.target).data("prop")] = $(event.target).val() === "null" ? null : $(event.target).val();
+        showSeries();
     })
 }
 
+let filters = {
+    az: null,
+    genreId: null,
+    dateFrom: null,
+    pageSize: 24,
+    page: 1
+};
+
+function removeEmpty(filters) {
+    let obj = {};
+    for(let key in filters) {
+        if(!filters.hasOwnProperty(key)) continue;
+        if(filters[key] != null) obj[key] = filters[key];
+    }
+    return obj;
+}
+
+async function showSeries(firstRun) {
+    NProgress.start();
+    let catalogue = await (await fetch("https://cors-anywhere.herokuapp.com/https://start-api.npo.nl/page/catalogue?" + $.param(removeEmpty(filters)), {
+        headers: {
+            "ApiKey": "e45fe473feaf42ad9a215007c6aa5e7e"
+        }
+    })).json();
+
+    if(firstRun) {
+        let filterAz = catalogue["components"][0]["filters"][0]["options"];
+        let filterAzContent = "";
+        filterAz.forEach(f => {
+            filterAzContent += `<option value="` + f.value + `">` + f.display + `</option>`
+        });
+        $("#filterAz").html(filterAzContent);
+
+        let filterGenre = catalogue["components"][0]["filters"][1]["options"];
+        let filterGenreContent = "";
+        filterGenre.forEach(f => {
+            filterGenreContent += `<option value="` + f.value + `">` + f.display + `</option>`
+        });
+        $("#filterGenre").html(filterGenreContent);
+
+        let filterMostWatched = catalogue["components"][0]["filters"][2]["options"];
+        let filterMostWatchedContent = "";
+        filterMostWatched.forEach(f => {
+            filterMostWatchedContent += `<option value="` + f.value + `">` + f.display + `</option>`
+        });
+        $("#filterMostWatched").html(filterMostWatchedContent);
+    }
+
+    let amountOfItems = catalogue["components"][1]["data"]["total"];
+    let pages = Math.ceil(amountOfItems / filters.pageSize);
+    let filterPageContent = "";
+    for(let i = 1; i <= pages; i++) {
+        filterPageContent += `<option value="` + i + `">` + i + `</option>`
+    }
+    $("#filterPage").html(filterPageContent);
+
+    let series = catalogue["components"][1]["data"]["items"];
+    let content = "";
+
+    for(let i = 0; i < series.length; i++) {
+        let serie = series[i];
+
+        let image = "";
+        if (serie.images != null && serie.images.header != null && serie.images.header.formats != null && serie.images.header.formats.web != null) {
+            image = serie.images.header.formats.web.source;
+        } else if (serie.images != null && serie.images.original != null && serie.images.original.formats != null && serie.images.original.formats.web != null) {
+            image = serie.images.original.formats.web.source;
+        }
+
+        content += `
+            <div class="col-md-2 series-item" data-id="` + serie.id + `">
+                    <div class="card mb-4 box-shadow series-show">
+                        <img class="card-img-top overview-image" src="` + image + `" alt="Series image" onerror="this.src='https://placehold.it/400x200'">
+                        <div class="card-body">
+                            <h5 class="card-title overflow">` + serie.title + `</h5>
+                            <p class="card-text"></p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <small class="text-muted overflow">` + serie.broadcasters.join(", ") + `</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+    }
+
+    $("#series").html(content);
+    NProgress.done();
+}
+
 load();
+showSeries(true);
