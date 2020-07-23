e93e514353c7f615ea58b7df6e745c1a95013105
Koen <koen@koenvh.nl>
2020-07-23 04:36:28 +0200

    Fix DR

--- src/app/Broadcaster/DR.php
+++ src/app/Broadcaster/DR.php
@@ -15,26 +15,42 @@ class DR extends Broadcaster
         $id = $output_array[1][0];
         $id = explode("_", $id);
         $id = end($id);
-        $response = $this->client->request("GET", $url);
-        preg_match_all('/"customId":"([^"]+)"/', $response->getBody(), $output_array);
-        $id = $output_array[1][0];
-        $id = explode(":", $id);
-        $id = end($id);
-        $response = $this->client->request("GET", "https://www.dr.dk/mu-online/api/1.4/programcard/?expanded=true&productionnumber=$id");
+
+
+        $response = $this->client->request("POST", "https://isl.dr-massive.com/api/authorization/anonymous-sso?device=web_browser&ff=idp%2Cldp&lang=da", [
+            "json" => [
+                "deviceId" => "dc72987a-bcb8-4ed1-a3a8-56ae4aaaf9b3",
+                "optout" => true,
+                "scopes" => ["Catalog"]
+            ]
+        ]);
         $data = json_decode($response->getBody(), true);
-        $video = $data["PrimaryAsset"]["Links"][1]["Uri"];
-        $subtitles = $data["PrimaryAsset"]["Subtitleslist"][0]["Uri"];
+        $key = $data[0]["value"];
 
-        if ($video == null) {
-            $encrypted = $data["PrimaryAsset"]["Links"][1]["EncryptedUri"];
-            $n = hexdec(substr($encrypted, 2, 8));
-            $a = substr($encrypted, 10 + $n);
-            $data = hex2bin(substr($encrypted, 10, $n));
-            $key = hex2bin(hash("sha256",  $a . ":sRBzYNXBzkKgnjj8pGtkACch"));
-            $iv = hex2bin($a);
+        $response = $this->client->request("GET", "https://isl.dr-massive.com/api/account/items/$id/videos?delivery=stream&device=web_browser&ff=idp%2Cldp&lang=da&resolution=HD-1080&sub=Anonymous", [
+            "headers" => [
+                "x-authorization" => "Bearer $key"
+            ]
+        ]);
+        $data = json_decode($response->getBody(), true);
+        $video = $data[0]["url"];
+        $subtitles = $data[0]["subtitles"][0]["link"];
 
-            $video = openssl_decrypt($data, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
-        }
+//        $response = $this->client->request("GET", "https://www.dr.dk/mu-online/api/1.4/programcard/?expanded=true&productionnumber=$id");
+//        $data = json_decode($response->getBody(), true);
+//        $video = $data["PrimaryAsset"]["Links"][1]["Uri"];
+//        $subtitles = $data["PrimaryAsset"]["Subtitleslist"][0]["Uri"];
+//
+//        if ($video == null) {
+//            $encrypted = $data["PrimaryAsset"]["Links"][1]["EncryptedUri"];
+//            $n = hexdec(substr($encrypted, 2, 8));
+//            $a = substr($encrypted, 10 + $n);
+//            $data = hex2bin(substr($encrypted, 10, $n));
+//            $key = hex2bin(hash("sha256",  $a . ":sRBzYNXBzkKgnjj8pGtkACch"));
+//            $iv = hex2bin($a);
+//
+//            $video = openssl_decrypt($data, 'aes-256-cbc', $key, OPENSSL_RAW_DATA, $iv);
+//        }
 
         return new StreamInformation($video, $subtitles);
     }
