fa7530a74d882d51caca4c9cf1dc9a2f304d1797
Koen <koen@koenvh.nl>
2020-10-25 18:11:54 +0100

    Fix SVT support

--- src/app/Broadcaster/SVT.php
+++ src/app/Broadcaster/SVT.php
@@ -12,14 +12,14 @@ class SVT extends Broadcaster
     function retrieve(string $url): StreamInformation
     {
         $response = $this->client->request("GET", $url);
-        preg_match_all('/data-video-id="(.+?)"/', $response->getBody()->getContents(), $output_array);
+        preg_match_all('/"svtId":\s?"(.+?)"/', $response->getBody()->getContents(), $output_array);
         $id = $output_array[1][0];
-        $response = $this->client->request("GET", "https://api.svt.se/videoplayer-api/video/$id");
+        $response = $this->client->request("GET", "https://api.svt.se/video/$id");
         $data = json_decode($response->getBody(), true);
         $video = "";
         $subtitles = "";
         foreach ($data["videoReferences"] as $item) {
-            if ($item["format"] == "dashhbbtv") {
+            if ($item["format"] == "dash-hbbtv") {
                 $video = $item["url"];
                 break;
             } elseif ($item["format"] == "hls") {
