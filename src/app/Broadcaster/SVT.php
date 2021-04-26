c7ceb153348a9d92fa53df3ffb34e04559397248
Koen <koen@koenvh.nl>
2021-04-26 02:25:24 +0200

    Fix SVT support

--- src/app/Broadcaster/SVT.php
+++ src/app/Broadcaster/SVT.php
@@ -11,22 +11,22 @@ class SVT extends Broadcaster
 
     function retrieve(string $url): StreamInformation
     {
-        if (strpos($url, "modalId") !== false) {
+        if (strpos($url, "id") !== false) {
             $urlComponents = parse_url($url);
             parse_str($urlComponents["query"], $params);
-            $id = $params["modalId"];
+            $id = $params["id"];
         } else {
             $response = $this->client->request("GET", $url);
             preg_match_all('/"videoSvtId\\\\":\\\\"(.+?)\\\\"/', $response->getBody()->getContents(), $output_array);
             $id = $output_array[1][0];
         }
         $response = $this->client->request("GET", "https://api.svt.se/video/$id");
-        $data = json_decode($response->getBody(), true);
+        $data = json_decode($response->getBody()->getContents(), true);
         $video = "";
         $subtitles = "";
 
         foreach ($data["videoReferences"] as $item) {
-            if ($item["format"] == "dash-hbbtv") {
+            if ($item["format"] == "dashhbbtv") {
                 $video = $item["url"];
                 break;
             } elseif ($item["format"] == "hls") {
--- src/app/Controller/TranslateController.php
+++ src/app/Controller/TranslateController.php
@@ -11,26 +11,6 @@ use Slim\Http\Response;
 
 class TranslateController
 {
-    function __invokeBackup(Request $request, Response $response, $args)
-    {
-        $params = json_decode(file_get_contents("php://input"), true);
-
-        $tr = new TranslateClient([
-            "key" => GOOGLE_TRANSLATE_KEY
-        ]);
-
-        $translated = $tr->translate($params["text"], [
-            "source" => $params["source"],
-            "target" => $params["target"]
-        ])["text"];
-
-        $response = $response->withHeader("Content-Type", "application/json");
-        $response->getBody()->write(json_encode([
-            "result" => $translated
-        ]));
-        return $response;
-    }
-
     function __invoke(Request $request, Response $response, $args)
     {
         $tr = new \Stichoza\GoogleTranslate\GoogleTranslate();
