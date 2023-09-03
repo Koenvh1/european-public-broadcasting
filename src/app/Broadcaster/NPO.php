e9f01f9e703d233666ecc278033170f7800d7657
Koen <koen@koenvh.nl>
2023-09-03 16:35:26 +0200

    Update NPO support

--- src/app/Broadcaster/NPO.php
+++ src/app/Broadcaster/NPO.php
@@ -17,25 +17,28 @@ class NPO extends Broadcaster
         $response = $this->client->request("GET", "https://www.npostart.nl/api/token", [
             "headers" => [
                 "X-Requested-With" => "XMLHttpRequest",
-                "X-Forwarded-For" => $_SERVER["REMOTE_ADDR"]
+                "X-Forwarded-For" => $_SERVER["REMOTE_ADDR"],
+                "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
             ]
         ]);
-        $token = json_decode($response->getBody()->getContents(), true)["token"];
-        $xsrfToken = $response->getHeader("Set-Cookie");
-        foreach ($xsrfToken as $item) {
-            if (preg_match('/npo_session=([^;]*);/', $item, $output_array)) {
+        $token = "";
+        $xsrfToken = "";
+        foreach ($response->getHeader("Set-Cookie") as $item) {
+            if (preg_match('/XSRF-TOKEN=([^;]*);/', $item, $output_array)) {
                 $xsrfToken = $output_array[1];
-                break;
+            }
+            if (preg_match('/npo_session=([^;]*);/', $item, $output_array)) {
+                $token = $output_array[1];
             }
         }
 
         $response = $this->client->request("POST", "https://www.npostart.nl/player/$videoId", [
             "headers" => [
                 "X-Requested-With" => "XMLHttpRequest",
-                "X-XSRF-TOKEN" => urldecode($xsrfToken),
+                "X-Xsrf-Token" => urldecode($xsrfToken),
                 "Content-Type" => "application/x-www-form-urlencoded",
-                "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0",
-                "Cookie" => "npo_session=$xsrfToken",
+                "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+                "Cookie" => "npo_session=$token",
                 "X-Forwarded-For" => $_SERVER["REMOTE_ADDR"]
             ],
             "form_params" => [
@@ -46,15 +49,29 @@ class NPO extends Broadcaster
                 "share" => "1",
                 "pageUrl" => "http://www.npostart.nl/nederland-van-boven/21-11-2013/VPWON_1184655",
                 "hasAdConsent" => "0",
-                "_token" => $token,
+//                "_token" => $token,
             ]
         ]);
-        $token = json_decode($response->getBody()->getContents(), true)["token"];
 
-        $response = $this->client->request("GET", "https://start-player.npo.nl/video/$videoId/streams?profile=dash-widevine&quality=npo&tokenId=$token&streamType=broadcast&mobile=0&ios=0&isChromecast=0", [
+        $player = json_decode($response->getBody()->getContents(), true);
+        $token = $player["token"];
+
+        $embed = $this->client->request("GET", $player["embedUrl"], [
             "headers" => [
-                "X-Forwarded-For" => $_SERVER["REMOTE_ADDR"]
+                "X-Forwarded-For" => $_SERVER["REMOTE_ADDR"],
+                "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
             ]
+        ]);
+
+        $response = $this->client->request("POST", "https://start-player.npo.nl/video/$videoId/streams?profile=dash-widevine&quality=npo&tokenId=$token&streamType=broadcast&isYospace=0&videoAgeRating=null&isChromecast=0&mobile=0&ios=0", [
+            "headers" => [
+                "X-Forwarded-For" => $_SERVER["REMOTE_ADDR"],
+                "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
+                "Host" => "start-player.npo.nl",
+                "Origin" => "https://start-player.npo.nl",
+                "Referer" => $player["embedUrl"]
+            ],
+            "http_errors" => false
         ]);
         $response = json_decode($response->getBody()->getContents(), true);
 
